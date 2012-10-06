fd4d03e4ea49895428a7d62917a28d22ff63af12
Lisa Danz <econerd4ever@gmail.com>
2012-10-06 11:36:25 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CandidateNewsAggregator::Application.config.secret_token = '54b0a39ab88359f587133bb9e223dc70ad78f775e85a5879df309130f0be90b981a8c784e47c1baf3433cdddfaf0aba6a8e04ef765bd7154a00384c30e66c297'
