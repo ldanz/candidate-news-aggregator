7b4ea4730ff2edaa3fb13f114dca24ac8c355d73
joofsh <jonathanpagano@gmail.com>
2012-10-06 17:34:01 -0400

    Change root routing to users#new

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -4,7 +4,8 @@ describe UsersController do
   describe "routing" do
 
     it 'routes users#new to root' do
-        root_path.should route_to(new_user_path)
+      get '/'
+       current_page.should route_to(new_user_path)
     end
   end
 end
