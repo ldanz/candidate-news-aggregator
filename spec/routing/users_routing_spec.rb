48f844c04924c895b3a4e628092eabab504c2da1
joofsh <jonathanpagano@gmail.com>
2012-10-06 17:49:33 -0400

    Remove root route rspec test

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -3,9 +3,6 @@ require "spec_helper"
 describe UsersController do
   describe "routing" do
 
-    it 'routes users#new to root' do
-      get '/'
-       current_page.should route_to(new_user_path)
     end
   end
 end
