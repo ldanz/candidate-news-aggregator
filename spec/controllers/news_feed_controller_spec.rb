f0c469552e31674a3e2299c39c3f18ae80b23fb4
Lisa Danz <econerd4ever@gmail.com>
2012-10-06 14:21:24 -0400

    fixed test

--- spec/controllers/news_feed_controller_spec.rb
+++ spec/controllers/news_feed_controller_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 describe NewsFeedController do
   describe 'GET new' do
-    get :new
-    response.should be_success
+    it 'should have a successful response' do
+      get :new
+      response.should be_success
+    end
   end
 end
