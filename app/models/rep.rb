ce7874bacb154aea24ac5ce9cde3a7a1eedc8632
Lisa Danz <econerd4ever@gmail.com>
2012-10-07 12:03:47 -0400

    added news to reps index page

--- app/models/rep.rb
+++ app/models/rep.rb
@@ -8,7 +8,7 @@ class Rep
   end
 
   def self.get_legislators_in zipcode
-    Sunlight::Legislator.all_in_zipcode(zipcode)
+    Sunlight::Legislator.all_in_zipcode(zipcode).map { |rep| Rep.new(name: "#{rep.firstname} #{rep.lastname}", title: rep.title, party: rep.party) }
   end
 
   def party
@@ -18,6 +18,14 @@ class Rep
       return "Republican"
     else
       return "Unknown"
+    end
+  end
+
+  def get_news
+    response = HTTParty.get("http://api.npr.org/query?fields=text&requiredAssets=text&searchTerm=#{URI::escape(name)}&dateType=story&output=JSON&apiKey=MDEwMjMzMzk3MDEzNDk2MjMxMDk0NjA0NA001")
+    stories = response['list']['story']
+    stories.map do |story|
+      story['text']['paragraph'].inject('') { |full_text, paragraph| "#{full_text}\n\n#{paragraph['$text']}"}
     end
   end
 end
