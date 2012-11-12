9d95a7dbb7b7e7526914a9149ef44d700c19166d
joofsh <jonathanpagano@gmail.com>
2012-11-11 23:00:34 -0500

    Refactor rep model to filter for the 3 user options

--- app/models/rep.rb
+++ app/models/rep.rb
@@ -1,6 +1,11 @@
 class Rep
 	attr_accessor :name, :title, :party, :district_num, :phone_number, :youtube_url
 
+    #npr api search id's for each topic
+    ECONOMY = '1017'
+    HEALTHCARE = '1027'
+    EDUCATION = '1013'
+
   def initialize(options = {})
     options.each do |attribute, value|
       self.send("#{attribute}=".to_sym, value)
@@ -21,18 +26,33 @@ class Rep
     end
   end
 
-  def get_news
-    #npr api search id's for each topic
-    ECON = 1017
-    HEALTHCARE = 1027
-    EDUCATION = 1013
+  def get_news filter = true
+    stories = api_news_call filter
+    if stories != nil
+      stories.map do |story|
+        title = story['title']['$text']
+        text = story['text']['paragraph'].map { |paragraph| paragraph['$text']}
+        {title: title, text: text}
+      end
+    end
+  end
 
-    response = HTTParty.get("http://api.npr.org/query?fields=text,title&requiredAssets=text&searchTerm=#{URI::escape(name)}&dateType=story&output=JSON&apiKey=#{NPR_API_KEY}")
-    stories = response['list']['story']
-    stories.map do |story|
-      title = story['title']['$text']
-      text = story['text']['paragraph'].map { |paragraph| paragraph['$text']}
-      {title: title, text: text}
+  def api_news_call filter
+    if filter == true
+      stories = []
+      [ECONOMY, HEALTHCARE, EDUCATION].each do |topic|
+        response = HTTParty.get("http://api.npr.org/query?#{topics_query_string topic}fields=text,title&requiredAssets=text&searchTerm=#{URI::escape(name)}&dateType=story&output=JSON&apiKey=#{NPR_API_KEY}")
+        if response['list']['story'] != nil
+          stories += response['list']['story']
+        end
+      end
+      stories
+    end
+  end
+
+  def topics_query_string topic
+    if topic != nil
+      "id=#{topic}&"
     end
   end
 end
